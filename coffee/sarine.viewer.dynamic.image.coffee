80e1c6d7c00c4b30238bdf3407d697bd6987729f
unknown <Adic@AdiC-pc.sarin.local>
2015-05-13 15:32:35 +0300

    changes for images

--- coffee/sarine.viewer.dynamic.image.coffee
+++ coffee/sarine.viewer.dynamic.image.coffee
@@ -1,12 +1,29 @@
 ###!
-sarine.viewer.dynamic.image - v0.1.0 -  Wednesday, May 13th, 2015, 1:46:22 PM 
+sarine.viewer.dynamic.image - v0.1.0 -  Wednesday, May 13th, 2015, 3:31:45 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
 ###
 class DynamicImage extends Viewer.Dynamic  
 
+	amountOfImages = undefined		
+	imagesArr = {}
+	downloadImagesArr = {}	
+	speed = undefined	
+	counter = 1
 	
 	constructor: (options) ->
-		super(options)								
+		super(options)	
+		{@sliceDownload, @backOnEnd, @imageType, @oneDigits, @speed, @amountOfImages} = options		
+		@sliceDownload = @sliceDownload || 3
+		@oneDigits = @oneDigits || true
+		@backOnEnd = @backOnEnd || false
+		@speed = @speed || 30
+		@amountOfImages = @amountOfImages || 200
+		@imagesArr = {}
+		@downloadImagesArr = {}
+		@first_init_defer = $.Deferred()
+		@full_init_defer = $.Deferred()
+		for index in [0..@amountOfImages]
+			@imagesArr[index] = undefined								
 
 	convertElement : () ->
 		@canvas = $("<canvas>")		
@@ -14,13 +31,51 @@ class DynamicImage extends Viewer.Dynamic
 		@element.append(@canvas)		
 
 	first_init : ()->
-		defer = $.Deferred()		
+		defer = @first_init_defer
+		defer.notify(@id + " : start load first image")
+		_t = @
+		
+		@loadImage(@src + 'img' + (if @oneDigits then "0" else "00") + (if @imageType then @imageType else ".png")).then((img)->
+			_t.canvas.attr {'width':img.width, 'height': img.height}
+			_t.ctx.drawImage img , 0 , 0 			
+			defer.resolve(_t) 
+		)
 		defer
+
+	loadParts : (gap,defer)->
+		gap = gap || 0
+		defer = defer || $.Deferred()
+		downloadImages = []
+		_t = @
+		$.when.apply($,for index in (index for index in Object.getOwnPropertyNames(@imagesArr) when (index + gap) % @sliceDownload == 0)
+			do (index)->
+				_t.loadImage(_t.src + 'img' + (if index < 10 and not _t.oneDigits  then "0" + index else index)  + (if _t.imageType then _t.imageType else ".png")).then((img)-> downloadImages.push img )
+			).then(()->
+					for img in downloadImages
+						do (img)->
+							index = parseInt(img.src.match(/\d+(?=.png|.jpg)/)[0])
+							downloadImagesArr[index] = imagesArr[index] = img
+					if Object.getOwnPropertyNames(imagesArr).length == (_t.amountOfImages + 1)
+						defer.resolve(_t)
+					else
+						_t.loadParts(++gap,defer)
+					_t.delay = (_t.sliceDownload / gap) * _t.speed  
+				)
+		return defer
+
 	
 	full_init : ()->
-		defer = $.Deferred()		
-		defer	
+		defer = @full_init_defer
+		defer.notify(@id + " : start load all images")
+		@loadParts().then(defer.resolve)  				
+		defer		
 
-		
+	nextImage : ()->
+		indexer = Object.getOwnPropertyNames(downloadImagesArr).map((v)-> parseInt(v)) 
+		if indexer.length > 1
+			@ctx.clearRect 0, 0, @ctx.canvas.width, @ctx.canvas.height
+			@ctx.drawImage downloadImagesArr[indexer[counter]] , 0 , 0	
+			# TODO use @backOnEnd to change direction
+			counter = (counter + 1) % indexer.length		
 
 @DynamicImage = DynamicImage
--- dist/sarine.viewer.dynamic.image.bundle.js
+++ dist/sarine.viewer.dynamic.image.bundle.js
@@ -1,6 +1,6 @@
 
 /*!
-sarine.viewer.dynamic.image - v0.1.0 -  Wednesday, May 13th, 2015, 1:46:22 PM 
+sarine.viewer.dynamic.image - v0.1.0 -  Wednesday, May 13th, 2015, 3:31:44 PM 
  The source code, name, and look and feel of the software are Copyright © 2015 Sarine Technologies Ltd. All Rights Reserved. You may not duplicate, copy, reuse, sell or otherwise exploit any portion of the code, content or visual design elements without express written permission from Sarine Technologies Ltd. The terms and conditions of the sarine.com website (http://sarine.com/terms-and-conditions/) apply to the access and use of this software.
  */
 
@@ -108,10 +108,36 @@ sarine.viewer.dynamic.image - v0.1.0 -  Wednesday, May 13th, 2015, 1:46:22 PM
   })(Viewer);
 
   DynamicImage = (function(_super) {
+    var amountOfImages, counter, downloadImagesArr, imagesArr, speed;
+
     __extends(DynamicImage, _super);
 
+    amountOfImages = void 0;
+
+    imagesArr = {};
+
+    downloadImagesArr = {};
+
+    speed = void 0;
+
+    counter = 1;
+
     function DynamicImage(options) {
+      var index, _i, _ref;
       DynamicImage.__super__.constructor.call(this, options);
+      this.sliceDownload = options.sliceDownload, this.backOnEnd = options.backOnEnd, this.imageType = options.imageType, this.oneDigits = options.oneDigits, this.speed = options.speed, this.amountOfImages = options.amountOfImages;
+      this.sliceDownload = this.sliceDownload || 3;
+      this.oneDigits = this.oneDigits || true;
+      this.backOnEnd = this.backOnEnd || false;
+      this.speed = this.speed || 30;
+      this.amountOfImages = this.amountOfImages || 200;
+      this.imagesArr = {};
+      this.downloadImagesArr = {};
+      this.first_init_defer = $.Deferred();
+      this.full_init_defer = $.Deferred();
+      for (index = _i = 0, _ref = this.amountOfImages; 0 <= _ref ? _i <= _ref : _i >= _ref; index = 0 <= _ref ? ++_i : --_i) {
+        this.imagesArr[index] = void 0;
+      }
     }
 
     DynamicImage.prototype.convertElement = function() {
@@ -121,15 +147,90 @@ sarine.viewer.dynamic.image - v0.1.0 -  Wednesday, May 13th, 2015, 1:46:22 PM
     };
 
     DynamicImage.prototype.first_init = function() {
-      var defer;
-      defer = $.Deferred();
+      var defer, _t;
+      defer = this.first_init_defer;
+      defer.notify(this.id + " : start load first image");
+      _t = this;
+      this.loadImage(this.src + 'img' + (this.oneDigits ? "0" : "00") + (this.imageType ? this.imageType : ".png")).then(function(img) {
+        _t.canvas.attr({
+          'width': img.width,
+          'height': img.height
+        });
+        _t.ctx.drawImage(img, 0, 0);
+        return defer.resolve(_t);
+      });
+      return defer;
+    };
+
+    DynamicImage.prototype.loadParts = function(gap, defer) {
+      var downloadImages, index, _t;
+      gap = gap || 0;
+      defer = defer || $.Deferred();
+      downloadImages = [];
+      _t = this;
+      $.when.apply($, (function() {
+        var _i, _len, _ref, _results;
+        _ref = (function() {
+          var _j, _len, _ref, _results1;
+          _ref = Object.getOwnPropertyNames(this.imagesArr);
+          _results1 = [];
+          for (_j = 0, _len = _ref.length; _j < _len; _j++) {
+            index = _ref[_j];
+            if ((index + gap) % this.sliceDownload === 0) {
+              _results1.push(index);
+            }
+          }
+          return _results1;
+        }).call(this);
+        _results = [];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          index = _ref[_i];
+          _results.push((function(index) {
+            return _t.loadImage(_t.src + 'img' + (index < 10 && !_t.oneDigits ? "0" + index : index) + (_t.imageType ? _t.imageType : ".png")).then(function(img) {
+              return downloadImages.push(img);
+            });
+          })(index));
+        }
+        return _results;
+      }).call(this)).then(function() {
+        var img, _fn, _i, _len;
+        _fn = function(img) {
+          var index;
+          index = parseInt(img.src.match(/\d+(?=.png|.jpg)/)[0]);
+          return downloadImagesArr[index] = imagesArr[index] = img;
+        };
+        for (_i = 0, _len = downloadImages.length; _i < _len; _i++) {
+          img = downloadImages[_i];
+          _fn(img);
+        }
+        if (Object.getOwnPropertyNames(imagesArr).length === (_t.amountOfImages + 1)) {
+          defer.resolve(_t);
+        } else {
+          _t.loadParts(++gap, defer);
+        }
+        return _t.delay = (_t.sliceDownload / gap) * _t.speed;
+      });
       return defer;
     };
 
     DynamicImage.prototype.full_init = function() {
       var defer;
-      defer = $.Deferred();
+      defer = this.full_init_defer;
+      defer.notify(this.id + " : start load all images");
+      this.loadParts().then(defer.resolve);
       return defer;
+    };
+
+    DynamicImage.prototype.nextImage = function() {
+      var indexer;
+      indexer = Object.getOwnPropertyNames(downloadImagesArr).map(function(v) {
+        return parseInt(v);
+      });
+      if (indexer.length > 1) {
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        this.ctx.drawImage(downloadImagesArr[indexer[counter]], 0, 0);
+        return counter = (counter + 1) % indexer.length;
+      }
     };
 
     return DynamicImage;
